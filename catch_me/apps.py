f504fe5cae7a0e1d441bb5b5e4d7c1dd020faba1
GeorgiKJoseph <georgikjoseph@gmail.com>
2020-03-29 16:00:38 +0530

    added: quickshare

--- catch_me/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CatchMeConfig(AppConfig):
-    name = 'catch_me'
---
+++ quickshare/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class QuickshareConfig(AppConfig):
+    name = 'quickshare'
