79c79a52a7d2e6df4c1005576147ecbc1628bbf2
GeorgiKJoseph <georgikjoseph@gmail.com>
2020-04-08 23:43:45 +0530

    added: ability to upload and store file (try1)

--- quickshare/models.py
+++ quickshare/models.py
@@ -6,6 +6,7 @@ class Card(models.Model):
     sender = models.CharField(max_length=32)
     header = models.CharField(max_length=64)
     text = models.TextField()
+    attachment = models.CharField(max_length=64)
     created_date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
--- quickshare/views.py
+++ quickshare/views.py
@@ -1,23 +1,46 @@
 from django.shortcuts import render
+from django.utils import timezone
+from django.core.files.storage import FileSystemStorage
+
 from .models import Card
 from .extras import system_info
 from .forms import CardForm
-from django.utils import timezone
+
 
 def home(request):
     info = request.META['HTTP_USER_AGENT']
     os, device = system_info(info)
-    # input form
-    if request.method == 'POST':
+#                                                                   input form
+    if request.method == 'POST':       
+        try:
+            uploaded_file = request.FILES['fil']
+            save(uploaded_file)
+            fileExists = True
+        except:
+            fileExists = False
+
+        #form = CardForm(request.POST, request.FILES)
         form = CardForm(request.POST)
         if form.is_valid():
             card = form.save(commit=False)
             card.sender = os + ' ' + device
             card.created_date = timezone.now()
+            if fileExists:
+                card.attachment = uploaded_file.name
+                fss = FileSystemStorage()
+                fss.save(uploaded_file.name,uploaded_file)
             card.save()
     form = CardForm()
+    
 
     # display content
     cards = Card.objects.all().order_by('-created_date')
     rtn = {'form': form, 'os':os,'device':device,'cards': cards}
     return render(request,'quickshare/home.html', rtn)
+
+'''
+def handle_uploaded_file(f):
+    with open('quickshare/files/test.txt','wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
+'''
